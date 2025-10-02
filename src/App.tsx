2c18c573ed56352d1c6e089d7caf4e529d0aa4c4
AqsaAslam786 <aqsaaslam1724@gmail.com>
2025-10-03 01:01:03 +0500

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
 
 import './App.css'
+import ProfileCard from './components/ProfileCard'
+
+export type ProfileCard = {
+  image: string,
+  name: string,
+  role: string,
+  linkedin: string,
+  github: string,
+  gmail: string,
+}
 
 function App() {
-  
+  const profile1: ProfileCard = {
+    image: "https://t4.ftcdn.net/jpg/06/38/71/61/360_F_638716104_ZaLUjHLnqDh5N8s8H4diKZLaAeB7qquq.webp",
+    name: "Aqsa Aslam",
+    role: "Software Engineer",
+    linkedin: "https://www.linkedin.com",
+    github: "https://www.github.com",
+    gmail: "aqsaaslam12334@gmail.com",
+  }
+
   return (
     <>
-      <div>Assalam u Alaikum </div>
+      
+
+      <Card  image={profile1.image} name={profile1.name} role={profile1.role} linkedin={profile1.linkedin} github={profile1.github} gmail={profile1.gmail} />
+      
     </>
   )
 }
